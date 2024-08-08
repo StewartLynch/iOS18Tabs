9215ba86fa464bd88dc7820f52eb1f2618770286
Stewart Lynch <slynch@createchsol.com>
2024-08-08 11:41:37 -0700

    Part1

--- iOS18Tabs/StartTabView.swift
+++ iOS18Tabs/StartTabView.swift
@@ -15,14 +15,48 @@
 import SwiftUI
 
 struct StartTabView: View {
+    
+    enum Tabs {
+        case home, family, passwords, search, news, notifications
+    }
+    
+    @State private var selection: Tabs = .home
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selection) {
+            Tab("Home", systemImage: "house", value: .home) {
+                VStack {
+                    Text("Home view")
+                    Button("Go Notifications") {
+                        selection = .notifications
+                    }
+                }
+            }
+            
+            Tab("Family", systemImage: "figure.and.child.holdinghands", value: .family) {
+                Text("Family View")
+            }
+            
+            Tab("Passwords", image: "security", value: .passwords) {
+                Text("My secure passwords")
+            }
+            
+            Tab(value: .search, role: .search) {
+                Text("Search View")
+            }
+            
+            Tab("News", systemImage: "newspaper", value: .news) {
+                Text("News View")
+            }
+            
+            Tab("Notifications", systemImage: "bell", value: .notifications) {
+                VStack {
+                    Text("Notifications View")
+                    Button("Go Home") {
+                        selection = .home
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
